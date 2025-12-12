468548aaf6d8fcc2b00056dbc68ab1ae0ee80f20
rajathshttgr <rajathshettigar.uy@gmail.com>
2025-12-12 09:33:36 +0530

    fix: CLI Response bug

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+namespace zoro {
+namespace config {
+    inline const std::string DEFAULT_DATA_PATH = "../../.data";
+}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
+#include "config.h"
 #include "storage/include/StorageEngine.h"
-#include "api/cli/CollectionService.h"
 #include "api/cli/CollectionService.h"
 #include "api/cli/cli.h"
 
 int main() {
-    zoro::storage::StorageEngine storage("../../.data");
+    zoro::storage::StorageEngine storage(zoro::config::DEFAULT_DATA_PATH);
     zoro::core::CollectionManager manager(&storage);
     zoro::api::CollectionService service(&manager);
     zoro::cli::Cli cli(&service);
