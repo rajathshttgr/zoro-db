41f4fef8a65831e4f72abcdb4bca744778c9045a
rajathshttgr <rajathshettigar.uy@gmail.com>
2026-04-07 23:57:50 +0530

    fix: refactor folder structure

--- src/services/README.md
+++ src/services/README.md
@@ -1,5 +1,12 @@
 # Services Module
 
+## Responsbilities
+
+- Strict business logic validations
+- Standard data format conversion
+- Metadata Cache lookup and rebuild
+- Timout and health monitor
+
 ## Build (Module-Level)
 
 ```bash
@@ -10,4 +17,19 @@ make -j8
 
 This builds the `service` static library:
 
----
+## Run Unit Tests (Module-Level)
+
+```bash
+mkdir build && cd build
+# Enable tests
+cmake .. -DBUILD_TESTS=ON
+make -j8
+```
+
+```bash
+ctest
+# or
+./unit_tests
+```
+
+> Note: Detailed unit tests and integration tests scripts and documentations are available in `tests` above instructions are only for quick testing and not recommended.
---
+++ src/services/src/points.cpp
@@ -0,0 +1,35 @@
+#include "./CollectionService.h"
+
+namespace zoro::services {
+
+bool CollectionService::UpsertPointsService(const std::string& coll_name, const std::vector<int>& point_id, const std::vector<std::vector<float>> &vectors, const std::vector<nlohmann::json>& payload) {
+    return manager_->UpsertPoints(coll_name, point_id, vectors, payload);
+}
+
+bool CollectionService::DeletePointsService(const std::string &coll_name, const std::vector<int> point_id) {
+    return manager_->DeletePoints(coll_name, point_id);
+}
+
+int CollectionService::CountPointsService(const std::string &coll_name) {
+    return manager_->CountPoints(coll_name);
+}
+
+std::optional<zoro::utils::PointInfo> CollectionService::RetrivePointById(const std::string& name, const int& point_id, std::string& err){
+    auto result = manager_->RetrivePointById(name, point_id, err);
+    if (!result.has_value()) {
+        err = "Matching point not found in collection.";
+        return std::nullopt;
+    }
+    return result;
+}
+
+std::vector<zoro::utils::ScrollPointInfo> CollectionService::ScrollPointMetadata(const std::string& name, const int limit, std::string& err){
+    if (limit <= 0) {
+        err = "limit should be atleast 1";
+        return {};
+    }
+    auto result= manager_->ScrollPointMetadata(name, limit, err);
+    return result;
+}
+
+}
