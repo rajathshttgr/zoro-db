4cfa3488ffa439d5e492634382b8d426db952b88
rajathshttgr <rajathshettigar.uy@gmail.com>
2026-04-07 23:58:46 +0530

    feat: metadata cache store

---
+++ src/services/src/metadata_cache.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+struct Metadata{
+    int size{};      
+    std::string distance{};   
+    std::string status{};
+};
+
+class MetadataCache {
+public:
+    MetadataCache(){};
+
+    bool get(const std::string &key, Metadata& result){
+        auto it = cacheMap.find(key);
+        if (it == cacheMap.end()) return false;
+
+        result = it->second;
+        return true;
+    };
+
+    bool put(const std::string &key, const Metadata &metadata){
+        cacheMap[key] = metadata;
+        return true;
+    };
+
+    void remove(const std::string &key){
+        auto it = cacheMap.find(key);
+        if (it == cacheMap.end()) return;
+        cacheMap.erase(it);
+    };
+
+private:
+    std::unordered_map<std::string, Metadata> cacheMap;
+};
